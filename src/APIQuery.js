5c991a7993c22e122a27949baa571b0255cfe20d
Toni Kangas <toni.ar.kangas@gmail.com>
2016-12-28 00:21:37 +0200

    Module to fetch departure data from digitransit.

---
+++ src/APIQuery.js
@@ -0,0 +1,54 @@
+//import fetch from 'react';
+
+var APIurl = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql"
+var query = "{ \
+  nearest (lat: 60.1836474999998, lon: 24.828072999999993, maxDistance: 150, filterByPlaceTypes: DEPARTURE_ROW) { \
+    edges { \
+      node { \
+        place { \
+          ... on DepartureRow { \
+            stop { \
+              name \
+              code \
+              platformCode \
+              desc \
+              lat \
+              lon \
+            } \
+            stoptimes { \
+              trip { \
+                route { \
+                  shortName \
+                  mode \
+                  alerts { \
+                    alertHeaderText \
+                    alertDescriptionText \
+                  } \
+                } \
+              } \
+              realtimeArrival \
+              realtime \
+              stopHeadsign \
+            } \
+          } \
+        } \
+      } \
+    } \
+  } \
+}" 
+
+var getNearestDepartures = function() {
+	console.log(query)
+	return fetch(APIurl, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/graphql'
+		},
+		body: query
+	})
+	.then((response) => response.json()) // console.log("Status: " + response.status); 
+	.then((responseJson) => { console.log(JSON.stringify(responseJson.data)); return responseJson.data; }) //console.log("JSON: " + JSON.stringify(responseJson));
+	.catch((error) => { console.error(error); });
+};
+
+export {getNearestDepartures};
