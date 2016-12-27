19d4f8f3149fb110ed952b56a2b1d67d8b5ec7ab
Toni Kangas <toni.ar.kangas@gmail.com>
2016-12-28 00:31:47 +0200

    Skeleton for App and print response JSON.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getNearestDepartures } from './APIQuery.js'
 
 class App extends Component {
-  render() {
-    return (
-      <h1>Hello world!</h1>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			data : {}
+		};
+	}
+
+	componentDidMount() {
+		var self = this;
+		getNearestDepartures()
+		.then((responseJson) => {self.setState({
+			data: responseJson
+		})});
+	}
+
+	render() {
+		return <p>{JSON.stringify(this.state.data)}</p>;
+	}
 }
 
 export default App;
