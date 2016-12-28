67e951956f97499b707fc466ef6c05bb923d40af
Toni Kangas <toni.ar.kangas@gmail.com>
2016-12-28 02:07:15 +0200

    Parse first list departure item.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import { getNearestDepartures } from './APIQuery.js'
 
+var parseTime = function(seconds, isRealTime){
+	isRealTime = isRealTime || false;
+	return (isRealTime ? ' ' : '~') + (~~(seconds/3600)).toString() + ':' + ((seconds%3600)/60).toString();
+}
+
 class App extends Component {
 	constructor(props) {
 		super(props);
@@ -19,7 +24,23 @@ class App extends Component {
 	}
 
 	render() {
-		return <p>{JSON.stringify(this.state.data)}</p>;
+		if (!this.state.data.hasOwnProperty('nearest')) {
+			return <p>No timetable data available.</p>;
+		}
+		var departureInfo = this.state.data.nearest.edges[3].node.place
+
+		return (
+			<ul>
+				<li class="route type"> {departureInfo.stoptimes[0].trip.route.mode} </li>
+				<li class="stop code"> {departureInfo.stop.name} </li>
+				<li class="stop name"> {departureInfo.stop.code} </li>
+				<li class="stop platform"> {departureInfo.stop.platformCode} </li>
+				<li class="route number"> {departureInfo.stoptimes[0].trip.route.shortName} </li>
+				<li class="route destination"> {departureInfo.stoptimes[0].stopHeadsign} </li>
+				<li class="route number"> {departureInfo.stoptimes[0].trip.route.shortName} </li>
+				<li class="route destination"> {parseTime(departureInfo.stoptimes[0].realtimeArrival, departureInfo.stoptimes[0].realtime)} </li>
+			</ul>
+		)
 	}
 }
 
