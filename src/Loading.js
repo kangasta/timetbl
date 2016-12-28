8a06fa009f203ecba54218679cb8b3fac4f60f88
Toni Kangas <toni.ar.kangas@gmail.com>
2016-12-28 20:11:11 +0200

    Separate loading info to separate files.

---
+++ src/Loading.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Loading.css';
+
+class Loading extends Component {
+	render() {
+		return <p>Loading timetable data.</p>;
+	}
+}
+
+export default Loading;
