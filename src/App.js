6daec60ec42fc0044b2a7dfc9ab7446347448f50
Toni Kangas <toni.ar.kangas@gmail.com>
2016-12-29 00:32:03 +0200

    Test stop depatures.

--- src/App.js
+++ src/App.js
@@ -14,26 +14,38 @@ class App extends Component {
 
 	componentDidMount() {
 		var self = this;
-		APIQuery.getNearestDepartures()
+		//APIQuery.getNearestDepartures()
+		APIQuery.getStopDepartures()
 		.then((responseJson) => {self.setState({
 			data: responseJson
 		})});
 	}
 
 	render() {
-		if (!this.state.data.hasOwnProperty('nearest')) {
+		if (!this.state.data.hasOwnProperty('stops')) {
+		//if (!this.state.data.hasOwnProperty('nearest')) {
 			return <Loading />;
 		}
-
-		var departureInfoArray = this.state.data.nearest.edges.filter(function(a){ return a.node.place.stoptimes.length > 0; });
+		var departureInfoArray = this.state.data.stops.filter(function(a){ return a.gtfsId.includes("HSL"); });
+		console.log(JSON.stringify(departureInfoArray, null, 2));
+		departureInfoArray[0].stoptimesWithoutPatterns.sort(function(a,b){
+			return (a.serviceDay - b.serviceDay) ?
+				(a.serviceDay - b.serviceDay) :
+				(a.realtimeArrival - b.realtimeArrival);
+			}
+		);
+		/*var departureInfoArray = this.state.data.nearest.edges.filter(function(a){ return a.node.place.stoptimes.length > 0; });
 		departureInfoArray.sort(function(a,b){
 			return (a.node.place.stoptimes[0].serviceDay - b.node.place.stoptimes[0].serviceDay) ?
 				(a.node.place.stoptimes[0].serviceDay - b.node.place.stoptimes[0].serviceDay) :
-				(a.node.place.stoptimes[0].realtimeArrival - b.node.place.stoptimes[0].realtimeArrival); });
+				(a.node.place.stoptimes[0].realtimeArrival - b.node.place.stoptimes[0].realtimeArrival);
+			}
+		);*/
 		return (
 			<div>
-				{departureInfoArray.map(function(departureInfoArrayItem, i){
-					return <DepartureInfo stop={departureInfoArrayItem.node.place.stop} stoptime={departureInfoArrayItem.node.place.stoptimes[0]} key={i}/>;
+				{departureInfoArray[0].stoptimesWithoutPatterns.map(function(departureInfoArrayItem, i){
+					return <DepartureInfo stoptime={departureInfoArrayItem} key={i}/>;
+					//return <DepartureInfo stop={departureInfoArrayItem.node.place.stop} stoptime={departureInfoArrayItem.node.place.stoptimes[0]} key={i}/>;
 				})}
 			</div>
 		)
