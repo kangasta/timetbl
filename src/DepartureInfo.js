9b64f8e787427f30cbc74cd6b27d3d477d9bfecf
Toni Kangas <toni.ar.kangas@gmail.com>
2016-12-28 20:45:38 +0200

    Set default props, fix props usage syntax.

--- src/DepartureInfo.js
+++ src/DepartureInfo.js
@@ -4,44 +4,51 @@ import './DepartureInfo.css';
 class DepartureInfo extends Component {
 	render() {
 		return (
-            <ul>
-				<li className="route type"> {info.stoptimes[0].trip.route.mode} </li>
-				<li className="stop code"> {info.stop.name} </li>
-				<li className="stop name"> {info.stop.code} </li>
-				<li className="stop platform"> {info.stop.platformCode} </li>
-				<li className="route number"> {info.stoptimes[0].trip.route.shortName} </li>
-				<li className="route destination"> {info.stoptimes[0].stopHeadsign} </li>
-				<li className="route number"> {info.stoptimes[0].trip.route.shortName} </li>
-				<li className="route destination"> {parseTime(info.stoptimes[0].realtimeArrival, info.stoptimes[0].realtime)} </li>
+			<ul>
+				<li className="route type"> {this.props.info.stoptimes[0].trip.route.mode} </li>
+				<li className="stop code"> {this.props.info.stop.name} </li>
+				<li className="stop name"> {this.props.info.stop.code} </li>
+				<li className="stop platform"> {this.props.info.stop.platformCode} </li>
+				<li className="route number"> {this.props.info.stoptimes[0].trip.route.shortName} </li>
+				<li className="route destination"> {this.props.info.stoptimes[0].stopHeadsign} </li>
+				<li className="route number"> {this.props.info.stoptimes[0].trip.route.shortName} </li>
+				<li className="route destination"> {this.parseTime(this.props.info.stoptimes[0].realtimeArrival, this.props.info.stoptimes[0].realtime)} </li>
 			</ul>
-        );
+		);
 	}
+
+    parseTime(seconds, isRealTime){
+        isRealTime = isRealTime || false;
+        return (isRealTime ? ' ' : '~') + (~~(seconds/3600)).toString() + ':' + ((seconds%3600)/60).toString();
+    }
 }
 
 DepartureInfo.defaultProps = {
-  "stop": {
-    "name": "Stop name",
-    "code": "Stop code",
-    "platformCode": "Platform code",
-    "desc": "Stop description",
-    "lat": 47.916667,
-    "lon": 106.916667
-  },
-  "stoptimes": [
-    {
-      "trip": {
-        "route": {
-          "shortName": "Route number",
-          "mode": "Route type",
-          "alerts": []
-        }
-      },
-      "realtimeArrival": 0,
-      "realtimeDeparture": 0,
-      "realtime": true,
-      "stopHeadsign": "Destination"
+    "info": {
+        "stop": {
+            "name": "Stop name",
+            "code": "Stop code",
+            "platformCode": "Platform code",
+            "desc": "Stop description",
+            "lat": 47.916667,
+            "lon": 106.916667
+        },
+        "stoptimes": [
+            {
+                "trip": {
+                    "route": {
+                        "shortName": "Route number",
+                        "mode": "Route type",
+                        "alerts": []
+                    }
+                },
+                "realtimeArrival": 0,
+                "realtimeDeparture": 0,
+                "realtime": true,
+                "stopHeadsign": "Destination"
+            }
+        ]
     }
-  ]
 };
 
-export default Loading;
+export default DepartureInfo;
