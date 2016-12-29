3f0f9281f71108d8bb0c8dc31e0612c7b1c34b5a
Toni Kangas <toni.ar.kangas@gmail.com>
2016-12-30 01:18:23 +0200

    Fix for loops.

--- src/APIQuery.test.js
+++ src/APIQuery.test.js
@@ -3,18 +3,18 @@ import APIQuery from './APIQuery.js'
 describe('APIQuery.queries', () => {
 
     it('should query at least name, code, platform, and location of stop', () => {
-        var stopFields = ["name", "code", "platformCode", "lat", "lon"];
-        for (var field in stopFields) {
-            expect(APIQuery.queries.nearestDepartures()).toMatch(field);
+        var fields = ["name", "code", "platformCode", "lat", "lon"];
+        for (var i = 0; i < fields; i++) {
+            expect(APIQuery.queries.nearestDepartures()).toMatch(fields[i]);
         }
     });
-    it('should query at least route, route type, destination, arrival time, departure time, real time, destination, and service day of stop', () => {
-        var stopFields = 
+    it('should query at least route, route type, destination, arrival time, departure time, real time, destination, and service day of stoptime', () => {
+        var fields = 
             [/trip.*{.*route.*{.*shortName.*}.*}/, /trip.*{.*route.*{.*mode.*}.*}/,
             "realtimeArrival", "realtimeDeparture", "realtime", "stopHeadsign", "serviceDay"];
-        for (var field in stopFields) {
-            expect(APIQuery.queries.nearestDepartures()).toMatch(field);
-            expect(APIQuery.queries.stopDepartures()).toMatch(field);
+        for (var i = 0; i < fields; i++) {
+            expect(APIQuery.queries.nearestDepartures()).toMatch(fields[i]);
+            expect(APIQuery.queries.stopDepartures()).toMatch(fields[i]);
         }
     });
 });
