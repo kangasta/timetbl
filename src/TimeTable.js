d600912ab6431436e9a75dfbc5dcc8d093deeff2
Toni Kangas <toni.ar.kangas@gmail.com>
2016-12-29 20:17:00 +0200

    Add new class for time tables.

---
+++ src/TimeTable.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './TimeTable.css';
+import Loading from './Loading.js'
+import DepartureInfo from './DepartureInfo.js'
+
+class TimeTable extends Component {
+	render() {
+		return <p>Loading timetable data.</p>;
+	}
+}
+
+export default TimeTable;
+
