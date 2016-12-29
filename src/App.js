66192ef0d96f2a3d23668444e809de887c0e65af
Toni Kangas <toni.ar.kangas@gmail.com>
2016-12-30 00:48:48 +0200

    Add implementation to show closest departures.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
+import Error from './Error.js'
+import Loading from './Loading.js'
 import TimeTable from './TimeTable.js'
+import UserLocation from './UserLocation.js'
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	componentDidMount() {
+		var self = this;
+		try {
+			UserLocation.getUserLocation((loc) => {
+				console.log("Got coordinates: " + loc.coords.latitude + "," + loc.coords.longitude);
+				self.setState({
+					lat:loc.coords.latitude, lon:loc.coords.longitude
+				});
+			}, (e) => { throw e; });
+		} catch (e) {
+			this.setState({error: {
+				name: "User location not available.",
+				message: e.toString()
+			}});
+		}
+	}
+
 	render() {
-		//return <TimeTable />;
-		//return <TimeTable stopCode="E2036" />;
-		return <TimeTable lat={60.1836474999998} lon={24.828072999999993} />;
+		if (this.state.hasOwnProperty("error")) {
+			return <Error name={this.state.error.name} message={this.state.error.msg.toString()}/>;
+		}
+		if (!this.state.hasOwnProperty("lat") || !this.state.hasOwnProperty("lat")) {
+			return <Loading />
+		}
+		return <TimeTable lat={this.state.lat} lon={this.state.lon} maxDistance={1000} />;
 	}
 }
 
