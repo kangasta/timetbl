08956e922fbd899d6b2761789d5437883379e833
Toni Kangas <toni.ar.kangas@gmail.com>
2016-12-30 00:47:23 +0200

    Add component for error messages.

---
+++ src/Error.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Error.css';
+
+class Error extends Component {
+	render() {
+        return (
+            <div className="error">
+                <h1>Error:</h1>
+                <h2>{this.props.name}</h2>
+                <p>{this.props.message}</p>
+            </div>
+        );
+	}
+}
+
+Error.defaultProps = {
+    header: "Error",
+    message: "Something went wrong. This is the default message. Blame the lazy developer for not giving you any more info."
+};
+
+export default Error;
