290f9a192b8af4b1bec8aec4cada2dc4f74b993c
Toni Kangas <toni.ar.kangas@gmail.com>
2016-12-30 03:00:57 +0200

    Add eslint rule and fix indent.

--- src/APIQuery.test.js
+++ src/APIQuery.test.js
@@ -10,7 +10,7 @@ describe('APIQuery.queries', () => {
 	it('should query at least route, route type, destination, arrival time, departure time, real time, destination, and service day of stoptime', () => {
 		var fields =
 			[/trip.*{.*route.*{.*shortName.*}.*}/, /trip.*{.*route.*{.*mode.*}.*}/,
-			'realtimeArrival', 'realtimeDeparture', 'realtime', 'stopHeadsign', 'serviceDay'];
+				'realtimeArrival', 'realtimeDeparture', 'realtime', 'stopHeadsign', 'serviceDay'];
 		for (var i = 0; i < fields; i++) {
 			expect(APIQuery.queries.nearestDepartures()).toMatch(fields[i]);
 			expect(APIQuery.queries.stopDepartures()).toMatch(fields[i]);
