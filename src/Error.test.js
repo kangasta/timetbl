be42fc61cc4fbdb96205fae7b41b4730ce120370
Toni Kangas <toni.ar.kangas@gmail.com>
2017-01-01 23:16:32 +0200

    Add enzyme tests for error and loading.

--- src/Error.test.js
+++ src/Error.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Error from './Error';
 
 describe('Error', () => {
 	it('renders without crashing', () => {
 		const div = document.createElement('div');
 		ReactDOM.render(<Error />, div);
+	});
+	it('prints given name and message', () => {
+		const component = shallow(<Error name='NAME' message='MESSAGE'/>);
+
+		expect(component.containsMatchingElement(<h2>NAME</h2>)).toEqual(true);
+		expect(component.containsMatchingElement(<p>MESSAGE</p>)).toEqual(true);
 	});
 });
--- src/Loading.test.js
+++ src/Loading.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Loading from './Loading';
 
 describe('Loading', () => {
 	it('renders without crashing', () => {
 		const div = document.createElement('div');
 		ReactDOM.render(<Loading />, div);
+	});
+	it('prints given name and message', () => {
+		const component = shallow(<Loading name='NAME' message='MESSAGE'/>);
+
+		expect(component.containsMatchingElement(<h2>NAME</h2>)).toEqual(true);
+		expect(component.containsMatchingElement(<p>MESSAGE</p>)).toEqual(true);
 	});
 });
