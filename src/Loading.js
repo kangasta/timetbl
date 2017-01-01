1dc06e37aa07f60780d1f3e74d2f58fa896d913e
Toni Kangas <toni.ar.kangas@gmail.com>
2017-01-01 22:12:54 +0200

    Add error style message and name.

--- src/Loading.js
+++ src/Loading.js
@@ -3,8 +3,24 @@ import './Loading.css';
 
 class Loading extends Component {
 	render() {
-		return <p>Loading timetable data.</p>;
+		return (
+			<div className='loading'>
+				<h1>Loading</h1>
+				<h2>{this.props.name}</h2>
+				<p>{this.props.message}</p>
+			</div>
+		);
 	}
 }
+
+Loading.defaultProps = {
+	name: 'Loading name',
+	message: 'Loading something. This is the default message. Blame the lazy developer for not giving you any more info and copy-pasting this text from Error screen.'
+};
+
+Loading.propTypes = {
+	name: React.PropTypes.string,
+	message: React.PropTypes.string
+};
 
 export default Loading;
