11de7f2f5db1b6107626a490cfd7f841a5f29568
Toni Kangas <toni.ar.kangas@gmail.com>
2017-01-04 01:53:39 +0200

    Test that departure infos are rendered.

--- src/__tests__/TimeTable.test.js
+++ src/__tests__/TimeTable.test.js
@@ -4,6 +4,9 @@ import { shallow } from 'enzyme';
 import TimeTable from './../TimeTable.js';
 import ErrorMsg from './../ErrorMsg';
 import LoadingMsg from './../LoadingMsg';
+import DepartureInfo from './../DepartureInfo.js';
+
+jest.mock('../APIQuery');
 
 describe('TimeTable', () => {
 	it('renders without crashing', () => {
@@ -17,5 +20,19 @@ describe('TimeTable', () => {
 	it('shows loading when created.', () => {
 		const component = shallow(<TimeTable lat={16.5} lon={28.5}/>);
 		expect(component.find(LoadingMsg)).toHaveLength(1);
+	});
+	it('shows nearest departure infos after succesfull API query.', async () => {
+		const component = shallow(<TimeTable lat={16.5} lon={28.5}/>);
+		await component.instance().sendQuery();
+		component.update();
+		expect(component.find(LoadingMsg)).toHaveLength(0);
+		expect(component.find(DepartureInfo)).not.toHaveLength(0);
+	});
+	it('shows stop departure infos after succesfull API query.', async () => {
+		const component = shallow(<TimeTable stopCode='E2036'/>);
+		await component.instance().sendQuery();
+		component.update();
+		expect(component.find(LoadingMsg)).toHaveLength(0);
+		expect(component.find(DepartureInfo)).not.toHaveLength(0);
 	});
 });
