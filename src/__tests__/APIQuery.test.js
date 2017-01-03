0da1efab108ee101c73449f1c69691811a0b2e8c
Toni Kangas <toni.ar.kangas@gmail.com>
2017-01-04 00:32:47 +0200

    Test get functions.

--- src/__tests__/APIQuery.test.js
+++ src/__tests__/APIQuery.test.js
@@ -1,13 +1,14 @@
 import APIQuery from './../APIQuery.js';
+import fetch from 'jest-fetch-mock';
 
-describe('APIQuery.queries', () => {
-	it('should query at least name, code, platform, and location of stop', () => {
+describe('APIQuery', () => {
+	it('queries at least name, code, platform, and location of stop', () => {
 		var fields = ['name', 'code', 'platformCode', 'lat', 'lon'];
 		for (var i = 0; i < fields; i++) {
 			expect(APIQuery.queries.nearestDepartures()).toMatch(fields[i]);
 		}
 	});
-	it('should query at least route, route type, destination, arrival time, departure time, real time, destination, and service day of stoptime', () => {
+	it('queries at least route, route type, destination, arrival time, departure time, real time, destination, and service day of stoptime', () => {
 		var fields =
 			[/trip.*{.*route.*{.*shortName.*}.*}/, /trip.*{.*route.*{.*mode.*}.*}/,
 				'realtimeArrival', 'realtimeDeparture', 'realtime', 'stopHeadsign', 'serviceDay'];
@@ -15,5 +16,23 @@ describe('APIQuery.queries', () => {
 			expect(APIQuery.queries.nearestDepartures()).toMatch(fields[i]);
 			expect(APIQuery.queries.stopDepartures()).toMatch(fields[i]);
 		}
+	});
+	it('returns promise with expected content for nearest query', () => {
+		const nearest = require('../__mocks__/NearestQueryResponse.json').data;
+		fetch.mockResponse(JSON.stringify(nearest));
+
+		APIQuery.getNearestDepartures()
+		.then((responseJson) => {
+			expect(responseJson).toEqual(nearest);
+		});
+	});
+	it('returns promise with expected content for stop query', () => {
+		const stop = require('../__mocks__/StopQueryResponse.json').data;
+		fetch.mockResponse(JSON.stringify(stop));
+
+		APIQuery.getStopDepartures()
+		.then((responseJson) => {
+			expect(responseJson).toEqual(stop);
+		});
 	});
 });
