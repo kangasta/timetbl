a1c670fc41c27c43c2488a49e60423bd3b1ca28e
Toni Kangas <toni.ar.kangas@gmail.com>
2017-01-05 01:49:11 +0200

    Rewrite api query mock to implement error behaviour.

--- src/__mocks__/APIQuery.js
+++ src/__mocks__/APIQuery.js
@@ -1,14 +1,40 @@
 class APIQuery {
-	static getNearestDepartures() {
-		return new Promise((resolve) => {
-			resolve(require('../__mocks__/NearestQueryResponse.json').data);
+	static nearestResponse = require('./NearestQueryResponse.json');
+	static stopResponse = require('./StopQueryResponse.json');
+	static invalidResponse = require('./InvalidQueryResponse.json');
+
+	static getNearestDepartures(lat=0, lon=0) {
+		return Promise.resolve(lat == 666 || lon == 666 ?  APIQuery.invalidResponse : APIQuery.nearestResponse)
+		.then((responseJson) => {
+			if (responseJson.hasOwnProperty('errors')){
+				throw Error('HSL API returned object with errors content instead of data\n:' + JSON.stringify(responseJson, null, 2));
+			}
+			return responseJson.data;
 		});
+		/*return new Promise((resolve, reject) => {
+			const responseJson = lat == 666 || lon == 666 ?  APIQuery.invalidResponse : APIQuery.nearestResponse;
+			if (responseJson.hasOwnProperty('errors')){
+				return reject('HSL API returned object with errors content instead of data\n:' + JSON.stringify(responseJson, null, 2));
+			}
+			return resolve(responseJson.data);
+		});*/
 	}
 
-	static getStopDepartures() {
-		return new Promise((resolve) => {
-			resolve(require('../__mocks__/StopQueryResponse.json').data);
+	static getStopDepartures(stopCode='E2036') {
+		return Promise.resolve(stopCode == '666' ?  APIQuery.invalidResponse : APIQuery.stopResponse)
+		.then((responseJson) => {
+			if (responseJson.hasOwnProperty('errors')){
+				throw Error('HSL API returned object with errors content instead of data\n:' + JSON.stringify(responseJson, null, 2));
+			}
+			return responseJson.data;
 		});
+		/*return new Promise((resolve, reject) => {
+			const responseJson = stopCode == '666' ?  APIQuery.invalidResponse : APIQuery.stopResponse;
+			if (responseJson.hasOwnProperty('errors')){
+				return reject('HSL API returned object with errors content instead of data\n:' + JSON.stringify(responseJson, null, 2));
+			}
+			resolve(responseJson.data);
+		});*/
 	}
 }
 
