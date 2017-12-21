54127b11674fea5668c8ee36931d4679fdc0ba44
Toni Kangas <toni.ar.kangas@gmail.com>
2017-12-22 01:28:20 +0200

    Remove url defined locations for now.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import TimeTable from './TimeTable.js';
 import UserLocation from './UserLocation.js';
 
-import { SFMainFeed, SFValidate } from './simple-feed/src/SF';
+import { SFMainFeed, SFGroup, SFValidate } from './simple-feed/src/SF';
 
 class App extends Component {
 	constructor(props) {
@@ -16,65 +16,17 @@ class App extends Component {
 	componentDidMount() {
 		var self = this;
 
-		var currentURL = window.location.href;
-
-		if (/kamppi/.exec(currentURL)) {
+		UserLocation.getUserLocation((loc) => {
 			self.setState({data: {
-				lat:60.169038, lon:24.932908, maxDistance:100, filterOut:'Leppävaara'
+				lat:Math.round(loc.coords.latitude*1e6)/1e6,
+				lon:Math.round(loc.coords.longitude*1e6)/1e6,
+				maxDistance:499
 			}});
-		}
-		else if (/kara/.exec(currentURL)) {
-			self.setState({data: {
-				lat:[60.224655, 60.215923],
-				lon:[24.759257, 24.753498],
-				maxDistance:[300, 100]
+		}, (error) => {
+			this.setState({data: {
+				error: error.toString()
 			}});
-		} else if (/keskusta/.exec(currentURL)) {
-			self.setState({data: {
-				lat:60.170508,
-				lon:24.941104,
-				maxDistance:300,
-			}});
-		} else if (/mattby/.exec(currentURL)) {
-			self.setState({data: {
-				lat:60.161874,
-				lon:24.739518,
-				maxDistance:1000,
-				filterOut:'Matinkylä'
-			}});
-		} else if (/niemi/.exec(currentURL)) {
-			self.setState({data: {
-				lat:60.186269,
-				lon:24.830909,
-				maxDistance:1000,
-				filterOut:'Otaniemi'
-			}});
-		} else if (/sello/.exec(currentURL)) {
-			self.setState({data: {
-				lat:60.219378,
-				lon:24.815121,
-				maxDistance:325,
-				filterOut:'Leppävaara'
-			}});
-		} else if (/niittari/.exec(currentURL)) {
-			self.setState({data: {
-				lat:[60.170882,60.167753],
-				lon:[24.760174,24.771770],
-				maxDistance:[300,150],
-			}});
-		} else  {
-			UserLocation.getUserLocation((loc) => {
-				self.setState({data: {
-					lat:Math.round(loc.coords.latitude*1e6)/1e6,
-					lon:Math.round(loc.coords.longitude*1e6)/1e6,
-					maxDistance:499
-				}});
-			}, (error) => {
-				this.setState({data: {
-					error: error.toString()
-				}});
-			});
-		}
+		});
 	}
 
 	render() {
@@ -83,7 +35,9 @@ class App extends Component {
 			return (
 				<div className='app'>
 					<SFMainFeed>
-						{SFValidate.generateErrorOrLoadingElement(this.state.data)}
+						<SFGroup head='Nearest departures:'>
+							{SFValidate.generateErrorOrLoadingElement(this.state.data)}
+						</SFGroup>
 					</SFMainFeed>
 				</div>
 			);
@@ -91,7 +45,7 @@ class App extends Component {
 		return(
 			<div className='app'>
 				<SFMainFeed>
-					<TimeTable lat={this.state.data.lat} lon={this.state.data.lon} maxDistance={this.state.data.maxDistance} maxResults={15} filterOut={this.state.data.filterOut}/>
+					<TimeTable head='Nearest departures:' lat={this.state.data.lat} lon={this.state.data.lon} maxDistance={this.state.data.maxDistance} maxResults={15} filterOut={this.state.data.filterOut}/>
 				</SFMainFeed>
 			</div>
 		);
