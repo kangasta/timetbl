199c72d8a27bedaf1a9839744300d58785bc97cc
Toni Kangas <toni.ar.kangas@gmail.com>
2019-01-15 22:15:17 +0200

    Add initial tests for title

--- src/__tests__/Title.test.js
+++ src/__tests__/Title.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Title from './../Title';
 
 describe('Title', () => {
 	it('renders without crashing', () => {
 		mount(<Title />);
+	});
+	it('displays clock if asked', () => {
+		[true, false].forEach(clock => {
+			const wrapper = shallow(<Title clock={clock}/>);
+
+			expect(wrapper.exists('.Clock')).toBe(clock);
+			if (clock) expect(wrapper.find('.Clock').text()).toMatch(/[0-9]{1,2}.[0-9]{2}/);
+		});
+	});
+	// TODO test that time is being updated
+	it('displays either coordinates or text', () => {
+		[
+			{text: 'Title', has: '.Code', hasNot: '.Coord'},
+			{text: 'Title', lat: 60, lon: 24, has: '.Code', hasNot: '.Coord'},
+			{lat: 60, lon: 24, has: '.Coord', hasNot: '.Code'}
+		].forEach(test => {
+			const wrapper = shallow(<Title text={test.text} lat={test.lat} lon={test.lon}/>);
+
+			expect(wrapper.exists(test.has)).toBe(true);
+			expect(wrapper.exists(test.hasNot)).toBe(false);
+		});
 	});
 });
